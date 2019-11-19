ec935488c8fd1372a97ee28010734119d94bf3ff
ecp-alex <alex.mackenzie@englishcollege.cz>
2019-11-19 22:45:29 +0100

    Footer | Styling

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,44 +1,23 @@
 import * as React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import TitleSection from "../TitleSection/TitleSection";
+import "./Contact.css";
 
 const contact = () => {
   return (
     <footer id="contact" className="bg-fourth">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-10 col-lg-offset-1 text-center">
-            <h2>Contact</h2>
-            <hr className="small" />
-            <h4>
-              <strong>Alex Mackenzie</strong>
-            </h4>
-            {/* <ul className="list-unstyled">
-              <li> */}
-            <FontAwesomeIcon icon={faEnvelope} />
-            <a href="mailto:alex@amactive.net">alex@amactive.net</a>
-            {/* </li>
-            </ul>
-            <br /> */}
-            {/* <!--<ul className="list-inline">
-                    <li>
-                        <a href="#"><i className="fa fa-facebook fa-fw fa-3x"></i></a>
-                    </li>
-                    <li>
-                        <a href="#"><i className="fa fa-twitter fa-fw fa-3x"></i></a>
-                    </li>
-                    <li>
-                        <a href="#"><i className="fa fa-dribbble fa-fw fa-3x"></i></a>
-                    </li>
-                </ul>--> */}
-            <hr className="small" />
-            <p className="text-muted">Copyright &copy; amactive 2019</p>
-          </div>
-        </div>
-      </div>
-      <a id="to-top" href="#top" className="btn btn-dark btn-lg">
-        <i className="fa fa-chevron-up fa-fw fa-1x"></i>
+      <TitleSection title="Contact" class="bg-fourth" />
+      <h4>Alex Mackenzie</h4>
+      <FontAwesomeIcon icon={faEnvelope} />
+      <a className="pad-icon-left" href="mailto:alex@amactive.net">
+        alex@amactive.net
       </a>
+      <hr className="dotted" />
+      <p className="copyright">Copyright &copy; amactive 2019</p>
+      {/* <a id="to-top" href="#top" className="btn btn-dark btn-lg">
+        <i className="fa fa-chevron-up fa-fw fa-1x"></i>
+      </a> */}
     </footer>
   );
 };
