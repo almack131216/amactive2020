cb837b45fdbca95d6d92bdce2ffa2570127bf709
ecp-alex <alex.mackenzie@englishcollege.cz>
2019-11-19 23:04:37 +0100

    ImgBanner | styling

--- src/components/ImgBanner/ImgBanner.tsx
+++ src/components/ImgBanner/ImgBanner.tsx
@@ -1,10 +1,7 @@
 import * as React from "react";
+import "./ImgBanner.css";
 
 class imgBanner extends React.Component<any, any> {
-  constructor(props: any) {
-    super(props);
-  }
-
   render() {
     return (
       <aside className="callout">
